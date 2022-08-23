bf91da0d2e0b7ecba7bdc96e6c0ec909881b8fb9
CarlosJuanBranco <jb8647866@gmail.com>
2022-08-23 13:47:39 -0300

    feat: Creating the Search page

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,6 +1,16 @@
+import { useState, useEffect } from 'react';
+import MovieCard from '../components/MovieCard';
+
+import { FaStar } from 'react-icons/fa';
+
+import './MoviesGrid.css';
+
 const Movie = () => {
     return (
-        <div> Movie </div>
+        <div>
+            <MovieCard />
+            <FaStar />
+        </div>
     )
 }
 
